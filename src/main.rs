d3da1ea4a871a743e7be6446332d1e9d10f56871
trojas-gnister <troy.sparks@worksheild.com>
2024-10-06 12:38:26 -0500

    wip

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,25 @@ use std::process::Command;
 use std::{thread, time::Duration};
 
 fn main() {
-    //TODO: Grab applications from user and provide as a selectable list using clap 
+   let application_list = Command::new("flatpak")
+       .arg("list")
+       .output()
+       .expect("Failed to execute flatpak list")
+
     let application = "com.brave.Browser";
     
     //TODO: do not depend on flatpak
-    Command::new("flatpak")
+    Command::neiw("flatpak")
         .arg("run")
         .arg(application)
         .spawn()
         .expect("Failed to launch application");
 
     //HACK: make sure app has launched
-    thread::sleep(Duration::from_secs(5));
-
-    
+    //thread::sleep(Duration::from_secs(5));
+//TODO grab wmctrl -l then check the id that has a name similar to the application selected 
+   //Command::new("wmctrl")
+   //    .args(["-r", application, "-b", "add,fullscreen"])
+       //.output()
+       //.expect("Failed to set the window to fullscreen");
 }
