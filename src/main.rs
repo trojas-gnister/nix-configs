c9fcf6770be3290368a9cb03d19d6591b0a14c16
trojas-gnister <iskrydev@gmail.com>
2024-10-07 12:49:06 -0500

    wip

--- src/main.rs
+++ src/main.rs
@@ -20,9 +20,28 @@ fn main() {
 
     let output_str = str::from_utf8(&command_output.stdout).expect("Invalid utf8");
 
-    //TODO: loop through apps and push apps into apps vec
+    //TODO: need to test if this is formatting correctly
     let mut apps: Vec<FlatpakApp> = Vec::new();
 
+    for line in output_str.lines().skip(1) {
+        let fields: Vec<&str> = line.split_whitespace().collect();
+
+        if fields.len() >= 5 {
+            let name = fields[0..fields.len() - 4].join("");
+            let application_id = fields[fields.len() - 4].to_string();
+            let version = fields[fields.len() - 3].to_string();
+            let branch = fields[fields.len() - 2].to_string();
+            let installation = fields[fields.len() - 1].to_string();
+
+            apps.push(FlatpakApp {
+                name,
+                application_id,
+                version,
+                branch,
+                installation,
+            });
+        }
+    }
     //TODO: allow user to select an app from the apps vec! using inquire
 
     // TODO: Launch app selected by the user
