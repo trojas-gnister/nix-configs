514a57527441486eff205ad4a031aa07beeac892
Troy <troy.sparks@workshield.com>
2024-11-22 19:31:33 -0600

    Update configuration.nix

--- app-nix-configs/development-ssh/configuration.nix
+++ app-nix-configs/development-ssh/configuration.nix
@@ -1,1 +1,24 @@
+{ config, lib, pkgs, ... }:
 
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "dev";
+  services.openssh.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    neovim
+    git
+    python3
+    tmux
+    pkgs.rustup
+  ];
+
+  system.stateVersion = "24.05";
+}
