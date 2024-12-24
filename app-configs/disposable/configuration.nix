c5165c29a0c2528a3bf3fa743cad560d3ebf6fc6
Troy <troy.sparks@workshield.com>
2024-12-24 04:59:33 +0000

    Update configuration.nix

--- app-configs/disposable/configuration.nix
+++ app-configs/disposable/configuration.nix
@@ -11,84 +11,50 @@ in
     (import "${home-manager}/nixos")
   ];
 
-  boot = {
-    loader = {
-      systemd-boot.enable = true;
-      efi.canTouchEfiVariables = true;
-    };
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
   };
 
-  networking = {
-    hostName = "disposable";
-  };
+  networking.hostName = "disposable";
 
-  time = {
-    timeZone = "America/Chicago";
-  };
+  time.timeZone = "America/Chicago";
 
   hardware = {
     firmware = [ pkgs.linux-firmware ];
-    bluetooth = {
-      enable = true;
-      powerOnBoot = true;
-    };
+    bluetooth.enable = true;
+    bluetooth.powerOnBoot = true;
   };
 
   services = {
-    blueman = {
+  resolved = {
+  enable = true;
+  };
+  mullvad-vpn.enable = true;
+  mullvad-vpn.package = pkgs.mullvad-vpn;
+    blueman.enable = true;
+    xserver.enable = true;
+    xserver.displayManager.defaultSession = "none+i3";
+    xserver.displayManager.autoLogin.enable = true;
+    xserver.displayManager.autoLogin.user = "nixos";
+    xserver.displayManager.sddm.enable = true;
+
+    xserver.windowManager.i3 = {
       enable = true;
+      extraPackages = with pkgs; [
+        dmenu
+        i3status
+        i3lock
+        i3blocks
+      ];
     };
 
-    xserver = {
-      enable = true;
-      displayManager = {
-        defaultSession = "none+i3";
-        autoLogin = {
-          enable = true;
-          user = "nixos";
-        };
-        sddm.enable = true;
-      };
-      windowManager = {
-        i3 = {
-          enable = true;
-          extraPackages = with pkgs; [
-            dmenu
-            i3status
-            i3lock
-            i3blocks
-          ];
-        };
-      };
-    };
-
-    openssh = {
-      enable = true;
-    };
-
-    spice-vdagentd = {
-      enable = true;
-    };
-
-    spice-autorandr = {
-      enable = true;
-    };
-
-    pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-      # If you want to use JACK applications, uncomment this
-      #jack.enable = true;
-    };
+    openssh.enable = true;
+    spice-vdagentd.enable = true;
+    spice-autorandr.enable = true;
   };
 
-  programs = {
-    git = {
-      enable = true;
-    };
-  };
+  programs.git.enable = true;
 
   environment = {
     systemPackages = with pkgs; [
@@ -101,72 +67,70 @@ in
       wl-clipboard
       openvpn
       kitty
-      pulseaudio
     ];
+
     variables = {
       GTK_THEME = "Adwaita:dark";
     };
+
     pathsToLink = [ "/libexec" ];
   };
 
-  users = {
-    users = {
-      nixos = {
-        isNormalUser = true;
-        uid = 1000;
-        group = "users";
-        extraGroups = [ "wheel" ];
-        home = "/home/nixos";
-        shell = pkgs.bash;
-      };
-    };
+  users.users.nixos = {
+    isNormalUser = true;
+    uid = 1000;
+    group = "users";
+    extraGroups = [ "wheel" ];
+    home = "/home/nixos";
+    shell = pkgs.bash;
   };
 
-  home-manager = {
-    users = {
-      nixos = { pkgs, ... }: {
-        home.stateVersion = "24.11";
+  home-manager.users.nixos = { pkgs, ... }: {
+    home.stateVersion = "24.11";
 
-        xsession.windowManager.i3 = {
-          enable = true;
-          config = {
-            terminal = "kitty";
-          };
-          extraConfig = ''
-            set $mod Mod1
-            font pango:DejaVu Sans Mono 8
-            floating_modifier $mod
-            exec librewolf
-            exec spice-vdagent -x -d
-            for_window [class="^.*"] border pixel 0
-          '';
-        };
+    xsession.windowManager.i3 = {
+      enable = true;
+      config = {
+      terminal = "kitty";
+      };
+      extraConfig = ''
+        set $mod Mod1
+        font pango:DejaVu Sans Mono 8
+        floating_modifier $mod
+        exec librewolf
+        exec spice-vdagent -x -d
+        for_window [class="^.*"] border pixel 0
+      '';
+    };
 
-        programs.librewolf = {
-          enable = true;
-          settings = {
-            "ui.systemUsesDarkTheme" = 1;
-          };
-        };
-
-        xdg = {
-          enable = true;
-          configFile."gtk-3.0/settings.ini".text = ''
-            [Settings]
-            gtk-theme-name = Adwaita-dark
-          '';
-        };
+    programs.librewolf = {
+      enable = true;
+      settings = {
+        "ui.systemUsesDarkTheme" = 1;
       };
     };
+
+    xdg = {
+      enable = true;
+      configFile."gtk-3.0/settings.ini".text = ''
+        [Settings]
+        gtk-theme-name = Adwaita-dark
+      '';
+    };
   };
+ 
+services.pipewire = {
+  enable = true;
+  alsa.enable = true;
+  alsa.support32Bit = true;
+  pulse.enable = true;
+  # If you want to use JACK applications, uncomment this
+  #jack.enable = true;
+};
 
   system = {
     stateVersion = "24.11";
-    activationScripts = {
-      runRfkill = {
-        text = "rfkill unblock bluetooth";
-      };
-    };
+
+    activationScripts.runRfkill.text = "rfkill unblock bluetooth";
   };
 }
-
