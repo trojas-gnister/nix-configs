80a192d2827b0db49d180460c095b0edf24a87a0
trojas-gnister <iskrydev@gmail.com>
2025-02-01 13:56:51 -0600

    added example variables file

---
+++ example.variables.nix
@@ -0,0 +1,11 @@
+#NOTE: REMOVE example from example.variables.nix to use
+{
+  userName = "your_username";
+  hostName = "your_hostname"; #SHOULD MATCH SELECTED CONFIGURATION
+  dataDevice = "if_mounting_device_for_gaming";
+  homeStateVersion = "home_version";
+  #READ THE DOCS
+  stateVersion = "initial_installed_state_version";
+  architecture = "system_architecture";
+}
+
