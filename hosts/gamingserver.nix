8b89e58d9fb84d4c978b6b6805db7b05ea9c468b
trojas-gnister <troy.sparks@workshield.com>
2025-07-02 20:47:46 -0500

    updated vm flakes

--- hosts/gamingserver.nix
+++ hosts/gamingserver.nix
@@ -8,8 +8,12 @@
   programs.adb.enable = true;
   networking.hostName = config.variables.networking.hostname;
   system.stateVersion = "24.11";
-
+  systemd.services."serial-getty@ttyS0" = {
+    enable = true;
+    wantedBy = [ "getty.target" ];
+  };
   boot = {
+    kernelParams = [ "console=ttyS0,115200" ];
     loader = {
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
