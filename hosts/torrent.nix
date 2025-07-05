12501ef69d30d59c456b0d1f5df83fd8186bb31f
trojas-gnister <troy.sparks@workshield.com>
2025-07-05 12:05:51 -0500

    updated torrent.nix for qemu console/ssh updates

--- hosts/torrent.nix
+++ hosts/torrent.nix
@@ -8,6 +8,9 @@
   programs.adb.enable = true;
   networking.hostName = config.variables.networking.hostname;
   system.stateVersion = "24.11";
+
+  services.qemuGuest.enable = true;
+
   systemd.services."serial-getty@ttyS0" = {
     enable = true;
     wantedBy = [ "getty.target" ];
